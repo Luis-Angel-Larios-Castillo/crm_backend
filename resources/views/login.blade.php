36cd66190905029ddcde63d65f960124dca6da61
LUIS ANGEL LARIOS CASTILLO <luis44larios.99@gmail.com>
2023-01-14 09:53:30 -0600

    Commit inicial

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+ <head>
+    <body>
+        <input type="text" placeholder="Usuario">
+        <input type="password" placeholder="Contrseña">
+        <input type="submit" value="Enviar">
+    </body>   
+<html>
